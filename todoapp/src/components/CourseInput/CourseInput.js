e681f1a05a06fff793cbff19791bcf8bdb6850ec
sanju <sanjayjanardhan231@gmail.com>
2024-07-01 11:37:23 +0530

    added Course_List

---
+++ todoapp/src/components/CourseInput/CourseInput.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label htmlFor="goalInput" style={{ color: isValid ? "black" : "red" }}>
+          Course Goal
+        </label>
+        <input
+          id="goalInput"
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            backgroundColor: isValid ? "transparent" : "lightpink",
+            borderColor: isValid ? "black" : "red",
+          }}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
